e78635501e281ec999b859be3c690912ecc5d18c
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-20 12:51:34 -0500

    added Understanding component and connected routers to it

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -6,6 +6,7 @@ import { HashRouter as Router, Route } from "react-router-dom";
 // Component imports:
 import Start from '../Start/Start';
 import Feeling from "../Feeling/Feeling";
+import Understanding from "../Understanding/Understanding";
 
 const Content = () => {
   return (
@@ -16,6 +17,9 @@ const Content = () => {
         </Route>
         <Route path="/feeling">
           <Feeling />
+        </Route>
+        <Route path="/understanding">
+          <Understanding />
         </Route>
       </Router>
     </div>
--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -6,16 +6,16 @@ import { Link } from "react-router-dom";
 // Chakra imports:
 import { Input } from "@chakra-ui/core";
 
-const Feeling = () => {
+const Understanding = () => {
   return (
     <div>
-      <h1>How are you feeling today?</h1>
+      <h1>How well are you understanding the content?</h1>
       <Input placeholder="1 - 10" size="lg" isRequired="true" />
-      <Link to="/understanding">
+      <Link>
         <button>NEXT</button>
       </Link>
     </div>
   );
 };
 
-export default Feeling;
+export default Understanding;
