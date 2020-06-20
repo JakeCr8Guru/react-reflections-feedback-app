85720b0636a1254f836e5719223d7b7256fcc955
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-20 09:20:16 -0500

    installed React Redux and plugged it in

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Import Redux:
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+const storeInstance = createStore(
+  combineReducers({}),
+  // redux devtools (browser extension): could also do redux logger here
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
