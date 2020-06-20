ba51b4f783170bbea4119583c72bf1c9812cbeb1
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-19 19:15:32 -0500

    added Footer component and npm installed Primer library

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 // Component imports:
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Layout = (props) => {
     return (
         <div>
             <Header />
             {props.children}
+            <Footer />
         </div>
     )
 }
