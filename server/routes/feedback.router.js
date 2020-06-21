e49207bf3d1d0ed1e2567b329a67302b6776423a
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-21 09:32:30 -0500

    setting up feedback router POST

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+// GET for DB
 router.get("/", (req, res) => {
   console.log("GET /api/feedback");
   pool
@@ -14,5 +15,32 @@ router.get("/", (req, res) => {
       res.sendStatus(500);
     });
 });
+
+// POST to DB
+router.post('/', (req, res) => {
+  const reflection = req.body;
+  console.log(reflection);
+  // Validation:
+	// if (reflection.feeling === '') {
+	// 	res.sendStatus(400);
+	// 	return;
+  // }
+  const queryText = 'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);';
+	pool.query(queryText, [
+			reflection.feeling,
+			reflection.understanding,
+      reflection.support,
+      reflection.comments,
+		])
+		.then((result) => {
+			console.log('finished posting!', result.rows);
+			res.status(201);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(500);
+		});
+});
+
 
 module.exports = router;
