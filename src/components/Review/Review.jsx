c82603ae36ce027a40e91da2ba99e18679b49e30
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-21 08:50:14 -0500

    added Redux global state to show in the Review component

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -4,20 +4,64 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // Chakra imports:
+import { Grid, Heading, Box, Button } from "@chakra-ui/core";
 
-const Review = () => {
+// Redux imports:
+import { connect } from "react-redux";
+
+const Review = (props) => {
   return (
-    <div>
-      <h1>Review Your Feedback</h1>
-      <h2>Feelings: 0</h2>
-      <h2>Understanding: 0</h2>
-      <h2>Support: 0</h2>
-      <h2>Comments: I have a comment...</h2>
-      <Link>
-        <button>SUBMIT</button>
-      </Link>
-    </div>
+    <Grid gridColumn="column">
+      <Box mb={4}>
+        <Heading as="h1" size="2xl">
+          Review Your Feedback
+        </Heading>
+      </Box>
+      <Box mb={4}>
+        <Heading as="h3" size="lg">
+          Feelings: {props.feeling}
+        </Heading>
+      </Box>
+      <Box mb={4}>
+        <Heading as="h3" size="lg">
+          Understanding: {props.understanding}
+        </Heading>
+      </Box>
+      <Box mb={4}>
+        <Heading as="h3" size="lg">
+          Support: {props.support}
+        </Heading>
+      </Box>
+      <Box mb={4}>
+        <Heading as="h3" size="lg">
+          Comments: {props.comments}
+        </Heading>
+      </Box>
+      <Box mt={4}>
+        <Link>
+          <Button
+            variantColor="green"
+            variant="outline"
+            size="lg"
+            height="48px"
+            width="200px"
+            border="2px"
+          >
+            SUBMIT
+          </Button>
+        </Link>
+      </Box>
+    </Grid>
   );
 };
 
-export default Review;
+const mapStateToProps = (state) => {
+  return {
+    feeling: state.feeling,
+    understanding: state.understanding,
+    support: state.support,
+    comments: state.comments,
+  };
+};
+
+export default connect(mapStateToProps)(Review);
