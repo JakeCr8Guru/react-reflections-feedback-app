27e48f41aed6f4100d29acb338550affc978eae2
Jake Schaffer <jakecr8guru@gmail.com>
2020-06-21 10:28:09 -0500

    POST is tested on PostMan it works ok now to settup axios on Review component

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -20,21 +20,53 @@ router.get("/", (req, res) => {
 router.post('/', (req, res) => {
   const reflection = req.body;
   console.log(reflection);
+  const { feeling, understanding, support, comments } = req.body;
+
   // Validation:
-	// if (reflection.feeling === '') {
-	// 	res.sendStatus(400);
-	// 	return;
-  // }
+  if (feeling === "" || understanding === "" || support === "") {
+    res
+      .status(400)
+      .send("A value came in as an empty string check input values");
+    return;
+  }
+
+  if (feeling === null || understanding === null || support === null) {
+    res
+      .status(400)
+      .send("A value came in as null check input values");
+    return;
+  }
+
+  if (feeling === undefined || understanding === undefined || support === undefined) {
+    res.status(400).send("A value came in as undefined check input values");
+    return;
+  }
+
+  if (
+    typeof feeling === "string" ||
+    typeof understanding === "string" ||
+    typeof support === "string"
+  ) {
+    Number(feeling);
+    Number(understanding);
+    Number(support);
+    return;
+  } else {
+    res
+      .status(400)
+      .send("typeof is not a number 400 error please fix something");
+  }
+
   const queryText = 'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);';
 	pool.query(queryText, [
-			reflection.feeling,
-			reflection.understanding,
-      reflection.support,
-      reflection.comments,
+			feeling,
+			understanding,
+      support,
+      comments,
 		])
 		.then((result) => {
 			console.log('finished posting!', result.rows);
-			res.status(201);
+			res.status(202);
 		})
 		.catch((err) => {
 			console.log(err);
